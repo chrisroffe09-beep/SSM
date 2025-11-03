968411614cd2a60f7e8e8736fd4eb8492e050b85
chrisroffe09-beep <chris.roffe09@gmail.com>
2025-11-03 18:53:56 -0500

    Copy main.py to /usr/local/bin during installation
    Add command to copy main.py to /usr/local/bin

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 # Make launcher executable
 chmod +x ssm
 
+sudo cp ssm_pkg/main.py /usr/local/bin/ssm_pkg/main.py
 # Copy launcher to /usr/local/bin
 echo "Installing SSM globally..."
 if sudo cp ssm /usr/local/bin/ssm; then
